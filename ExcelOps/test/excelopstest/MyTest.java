cd5eab21d23db5d34ca696c37d9c9c0a234aeb95
Jeff Shepherd <jeffindenver2002@yahoo.com>
2018-12-06 13:57:53 -0600

    Organized imports

--- ExcelOps/test/excelopstest/MyTest.java
+++ ExcelOps/test/excelopstest/MyTest.java
@@ -4,10 +4,10 @@ import excelops.ExcelOps;
 import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
