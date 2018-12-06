7bad2bc3f7b97d5151070a94fc81409188076d49
Jeff Shepherd <jeffindenver2002@yahoo.com>
2018-12-06 13:57:14 -0600

    Initial commit

---
+++ ExcelOps/test/excelopstest/MyTest.java
@@ -0,0 +1,58 @@
+package excelopstest;
+
+import excelops.ExcelOps;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author JShepherd
+ */
+public class MyTest {
+    
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    private ExcelOps excelOps;
+    private List<String> list;
+    private String filename;
+    
+    public MyTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        filename = "CellOne Nov 25.xlsx";
+        excelOps = new ExcelOps(filename);
+        list = excelOps.getList();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testReadWorkbook() {
+        assertEquals("CellOne Nov 25.xlsx", filename);
+        assertNotNull("Failed: excelOps is null", excelOps);        
+    }
+    
+    @Test
+    public void testWorkbookToList() {
+        assertNotNull("Failed: list is null", list);
+        excelOps.printList(list);
+
+    }
+
+}
