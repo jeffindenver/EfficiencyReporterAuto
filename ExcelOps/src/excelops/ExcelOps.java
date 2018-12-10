4bee578f4efa2a33116d7a312745903fd63573bd
Jeff Shepherd <jeffindenver2002@yahoo.com>
2018-12-10 14:20:41 -0600

    Changed method to throw exceptions instead of handling

--- ExcelOps/src/excelops/ExcelOps.java
+++ ExcelOps/src/excelops/ExcelOps.java
@@ -6,11 +6,11 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.StringJoiner;
-import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbookFactory;
@@ -22,33 +22,37 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbookFactory;
 public class ExcelOps {
 
     private final List<String> list;
+    private XSSFWorkbook workbook;
 
-    public ExcelOps(String filename) {
+    public ExcelOps() {
         list = new ArrayList<>();
-        readWorkbook(filename);
+
     }
 
-    private void readWorkbook(String filename) {
-        XSSFWorkbook wb = null;
+    public XSSFWorkbook openWorkbook(String filename) throws InvalidFormatException, IOException {
+        //if the workbook does not already exist, a new workbook is created.
         File file = new File(filename);
+        XSSFWorkbook wb = null;
 
         try {
             wb = XSSFWorkbookFactory.createWorkbook(file, true);
-            toList(wb);
-        } catch (InvalidFormatException | IOException | EncryptedDocumentException e) {
+        } catch (InvalidFormatException | IOException e) {
             System.out.println(e.getMessage());
+            throw e;
         } finally {
             if (wb != null) {
                 try {
                     wb.close();
                 } catch (IOException e) {
                     System.out.println(e.getMessage());
+                    throw e;
                 }
             }
         }
+        return wb;
     }
 
-    private void toList(XSSFWorkbook wb) {
+    public void toList(XSSFWorkbook wb) {
         XSSFSheet sheet = wb.getSheetAt(0);
         Iterator<Row> rowIterator = sheet.iterator();
 
@@ -70,6 +74,15 @@ public class ExcelOps {
         }
     }
 
+    private void writeSheet() {
+        int endSheet = workbook.getNumberOfSheets();
+        XSSFSheet newSheet = workbook.cloneSheet(endSheet);
+        XSSFRow row = newSheet.getRow(5);
+        //todo clear contents of all rows in separate method
+        //write new data to correct range. Add row range an argument to this
+        //method.
+    }
+
     public void printList(List<String> list) {
         for (String s : list) {
             System.out.println(s);
@@ -80,5 +93,12 @@ public class ExcelOps {
         return new ArrayList<>(list);
     }
 
+    public void setWorkbook(XSSFWorkbook wb) {
+        workbook = wb;
+    }
+
+    public XSSFWorkbook getWorkbook() {
+        return workbook;
+    }
 
 }
