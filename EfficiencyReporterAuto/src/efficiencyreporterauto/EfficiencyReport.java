52701809b5cc415d6ffca07b979e50a0ebff61c2
Jeff Shepherd <jeffindenver2002@yahoo.com>
2018-12-10 13:11:00 -0600

    Added description of program

--- EfficiencyReporterAuto/src/efficiencyreporterauto/EfficiencyReport.java
+++ EfficiencyReporterAuto/src/efficiencyreporterauto/EfficiencyReport.java
@@ -1,8 +1,9 @@
 package efficiencyreporterauto;
 
-
-
 /**
+ * This is a second version of "EfficiencyReport" application. This version will 
+ * read .xlsx/.xls files directly and then modifying or creating the same. 
+ * Previous version has .csv input and output.
  *
  * @author JShepherd
  */
