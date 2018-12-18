d0514f8fe30fd4d76153214fdadeec6435bdf647
Jeff Shepherd <jeffindenver2002@yahoo.com>
2018-12-18 14:06:07 -0600

    Added extractDate() function to parse date from third row of the report. Added determineOutputTarget(filename) function to determine if an existing file should be modified or a new file created. Added method to write to an existing xlsx file

--- EfficiencyReporterAuto/src/efficiencyreporterauto/Controller.java
+++ EfficiencyReporterAuto/src/efficiencyreporterauto/Controller.java
@@ -7,6 +7,7 @@ import java.awt.dnd.DropTarget;
 import java.awt.dnd.DropTargetDropEvent;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
@@ -39,7 +40,8 @@ public class Controller {
 
                     @SuppressWarnings("unchecked")
                     List<File> droppedFiles
-                            = (List<File>) evt.getTransferable().getTransferData(DataFlavor.javaFileListFlavor);
+                            = (List<File>) evt.getTransferable()
+                                    .getTransferData(DataFlavor.javaFileListFlavor);
 
                     for (File file : droppedFiles) {
                         view.printMessage(file.toString());
@@ -51,14 +53,17 @@ public class Controller {
             }
 
             private void processFile(String filename) {
-
                 getSourceList(filename);
+
+                extractDate();
 
                 initializeAgents();
 
                 cleanData();
 
                 calculateStats();
+
+                writeToExistingFile(determineOutputTarget(filename));
 
                 writeCSVFile(filename);
 
@@ -75,6 +80,66 @@ public class Controller {
             view.printError(e.getMessage());
         }
     }
+        
+    private void writeToExistingFile(String filename) {
+        try {
+            model.writeToExistingFile(filename);
+        } catch (InvalidFormatException | IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    private String determineOutputTarget(String filename) {
+        String targetFile = filename;
+        List<String> workgroupNames = Arrays.asList("CellOne", "Drobo",
+                "Homesnap", "Newmark", "Orbit", "Shared", "Xplore", "YKHC");
+        for (String name : workgroupNames) {
+            if (filename.contains(name)) {
+                targetFile = getExistingFilename(name);
+            }
+        }
+        return targetFile;
+    }
+
+    private String getExistingFilename(String name) {
+        String translation = "S:\\Reports\\Efficiency Reports\\";
+        switch (name) {
+            case "CellOne":
+                translation += "CellOne Efficiency 2018.xlsx";
+                break;
+            case "Drobo":
+                translation += "Drobo Efficiency 2018.xlsx";
+                break;
+            case "Homesnap":
+                translation += "Homesnap Efficiency 2018.xlsx";
+                break;
+            case "Newmark":
+                translation += "Newmark Efficiency 2018.xlsx";
+                break;
+            case "Orbit":
+                translation += "Orbit Efficiency 2018.xlsx";
+                break;
+            case "Shared":
+                translation += "Shared Support Efficiency 2018.xlsx";
+                break;
+            case "Xplore":
+                translation += "Xplore Efficiency 2018.xlsx";
+                break;
+            case "YKHC":
+                translation += "YKHC Efficiency 2018.xlsx";
+                break;
+            default:
+                translation += "new efficiency report.xlsx";
+        }
+        return translation;
+    }
+
+
+
+    private void extractDate() {
+        model.extractDate();
+    }
 
     private void initializeAgents() {
         model.initializeAgents();
@@ -86,12 +151,15 @@ public class Controller {
 
     private void writeCSVFile(String aName) {
         File file = new File(model.composeFilepath(aName, ".csv"));
-        model.writeListToFile(file);
+        try {
+            model.writeListToFile(file);            
+        } catch (IOException e) {
+            view.printError(e.getMessage());
+        }
     }
 
     private void writeExcelFile(String aName) {
-        File file = new File(model.composeFilepath(aName, ".xlsx"));
-        model.writeListToXlsxFile(file);
+        model.writeListToXlsxFile(model.composeFilepath(aName, ".xlsx"));
     }
 
     private void cleanData() {
