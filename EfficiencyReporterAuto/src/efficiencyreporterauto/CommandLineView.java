c082a27ffdfab82a97ae6e4f390bc38968656f3a
Jeff Shepherd <jeffindenver2002@yahoo.com>
2018-12-28 08:04:38 -0600

    Renamed View class to CommandLineView

---
+++ EfficiencyReporterAuto/src/efficiencyreporterauto/CommandLineView.java
@@ -0,0 +1,34 @@
+package efficiencyreporterauto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author JShepherd
+ */
+public class CommandLineView {
+    //class to communicate with user, accept input
+
+    void printError(String errorMessage) {
+        System.err.println(errorMessage);
+    }
+
+    void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public String getUserInput(String prompt) {
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        System.out.println(prompt);
+        if (sc.hasNextLine()) {
+            input = sc.nextLine();
+        }
+        return input;
+    }
+
+    @Override
+    public String toString() {
+        return "Command line interface";
+    }
+}
